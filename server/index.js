311545013861d788253fe00c76d6d0bbfc5e4913
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-05-01 13:06:22 +0200

    Fix bug

--- server/index.js
+++ server/index.js
@@ -9,17 +9,22 @@ dotenv.config();
 
 const app = express();
 
-app.use('/posts', postRoutes);
-
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
+app.use('/posts', postRoutes);
+
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(process.env.CONNECTION_URL)
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then(() =>
     app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
   )
   .catch(error => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
