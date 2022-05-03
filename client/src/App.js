67a4190bfc95f892d859fa7305fe721608122022
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-05-03 15:20:19 +0200

    Share state of currentId between Posts and Form

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
 import memories from './images/memories.png';
@@ -8,6 +8,7 @@ import useStyles from './styles';
 import { getPosts } from './actions/posts';
 
 const App = () => {
+  const [currentId, setCurrentId] = useState(null);
   const classes = useStyles();
   const dispatch = useDispatch();
 
@@ -37,10 +38,10 @@ const App = () => {
             spacing={3}
           >
             <Grid item xs={12} sm={7}>
-              <Posts />
+              <Posts setCurrentId={setCurrentId} />
             </Grid>
             <Grid item xs={12} sm={4}>
-              <Form />
+              <Form currentId setCurrentId={setCurrentId} />
             </Grid>
           </Grid>
         </Container>
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -3,7 +3,7 @@ import { Grid, CircularProgress } from '@material-ui/core';
 import { useSelector } from 'react-redux';
 import Post from './Post/Post';
 
-const Posts = () => {
+const Posts = ({ setCurrentId }) => {
   const posts = useSelector(state => state.posts);
 
   return !posts.length ? (
@@ -12,7 +12,7 @@ const Posts = () => {
     <Grid container alignItems="stretch" spacing={3}>
       {posts.map(post => (
         <Grid key={post._id} item xs={12} sm={6} md={6}>
-          <Post post={post} />
+          <Post post={post} setCurrentId={setCurrentId} />
         </Grid>
       ))}
     </Grid>
