632ffc06a4d9fc78586a82f69f45deb0c98ec7e8
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-05-04 13:22:26 +0200

    Dispatch updatePost

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -3,7 +3,7 @@ import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import FileBase from 'react-file-base64';
 import { useDispatch } from 'react-redux';
 import useStyles from './styles';
-import { createPost } from '../../actions/posts';
+import { createPost, updatePost } from '../../actions/posts';
 
 const Form = ({ currentId, setCurrentId }) => {
   const [postData, setPostData] = useState({
@@ -19,7 +19,11 @@ const Form = ({ currentId, setCurrentId }) => {
   const handleSubmit = e => {
     e.preventDefault();
 
-    dispatch(createPost(postData));
+    if (currentId) {
+      dispatch(updatePost(currentId, postData));
+    } else {
+      dispatch(createPost(postData));
+    }
   };
 
   const clear = () => {};
