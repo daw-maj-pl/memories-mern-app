c5ff065251a38959089aa463bb4bf57fa03bc737
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-05-12 13:04:29 +0200

    Implement handleChange and handleSubmit methods to set state and dispatch actions

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import {
   Avatar,
   Button,
@@ -11,17 +13,38 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import useStyles from './styles';
 import Input from './Input';
 import GoogleLogin from './GoogleLogin';
+import { signin, signup } from '../../actions/auth';
+
+const initialState = {
+  firstName: '',
+  lastName: '',
+  email: '',
+  password: '',
+  confirmPassword: ''
+};
 
 const Auth = () => {
   const classes = useStyles();
   const [showPassword, setShowPassword] = useState(false);
   const [isSignup, setIsSignup] = useState(false);
+  const [formData, setFormData] = useState(initialState);
+  const dispatch = useDispatch();
+  const history = useHistory();
 
   const handleShowPassword = () => setShowPassword(!showPassword);
 
-  const handleSubmit = () => {};
+  const handleSubmit = e => {
+    e.preventDefault();
 
-  const handleChange = () => {};
+    if (isSignup) {
+      dispatch(signup(formData, history));
+    } else {
+      dispatch(signin(formData, history));
+    }
+  };
+
+  const handleChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
 
   const switchMode = () => {
     setIsSignup(prevIsSignup => !prevIsSignup);
