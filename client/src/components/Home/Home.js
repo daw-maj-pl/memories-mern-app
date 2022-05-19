92f06b50f50d98225465a8d26d41ad815bc40682
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-05-19 22:15:54 +0200

    Add searching fields

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,24 +1,66 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { Container, Grow, Grid, Paper } from '@material-ui/core';
-import { getPosts } from '../../actions/posts';
+import { useHistory, useLocation } from 'react-router-dom';
+import {
+  Container,
+  Grow,
+  Grid,
+  Paper,
+  AppBar,
+  TextField,
+  Button
+} from '@material-ui/core';
+import ChipInput from 'material-ui-chip-input';
+import { getPosts, getPostsBySearch } from '../../actions/posts';
 import Pagination from '../Pagination';
 import Posts from '../Posts/Posts';
 import Form from '../Form/Form';
 import useStyles from './styles';
 
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
+
 const Home = () => {
   const [currentId, setCurrentId] = useState(null);
+  const [search, setSearch] = useState('');
+  const [tags, setTags] = useState([]);
   const classes = useStyles();
   const dispatch = useDispatch();
+  const query = useQuery();
+  const history = useHistory();
+  const page = query.get('page') || 1;
+  const searchQuery = query.get('searchQuery');
 
   useEffect(() => {
     dispatch(getPosts());
   }, [dispatch]);
 
+  const searchPost = () => {
+    if (search.trim() || tags) {
+      dispatch(getPostsBySearch({ search, tags: tags.join(',') }));
+      history.push(
+        `/posts/search?searchQuery=${search || 'none'}&tags=${tags.join(',')}`
+      );
+    } else {
+      history.push('/');
+    }
+  };
+
+  const handleKeyPress = e => {
+    if (e.keyCode === 13) {
+      searchPost();
+    }
+  };
+
+  const handleAddChip = tag => setTags([...tags, tag]);
+
+  const handleDeleteChip = chipToDelete =>
+    setTags(tags.filter(tag => tag !== chipToDelete));
+
   return (
     <Grow in>
-      <Container>
+      <Container maxWidth="xl">
         <Grid
           className={classes.mainContainer}
           container
@@ -26,10 +68,41 @@ const Home = () => {
           alignItems="stretch"
           spacing={3}
         >
-          <Grid item xs={12} sm={7}>
+          <Grid item xs={12} sm={6} md={9}>
             <Posts setCurrentId={setCurrentId} />
           </Grid>
-          <Grid item xs={12} sm={4}>
+          <Grid item xs={12} sm={6} md={3}>
+            <AppBar
+              className={classes.appBarSearch}
+              position="static"
+              color="inherit"
+            >
+              <TextField
+                onKeyDown={handleKeyPress}
+                name="search"
+                variant="outlined"
+                label="Search Memories"
+                fullWidth
+                value={search}
+                onChange={e => setSearch(e.target.value)}
+              />
+              <ChipInput
+                style={{ margin: '10px 0' }}
+                value={tags}
+                onAdd={chip => handleAddChip(chip)}
+                onDelete={chip => handleDeleteChip(chip)}
+                label="Search Tags"
+                variant="outlined"
+              />
+              <Button
+                onClick={searchPost}
+                className={classes.searchButton}
+                variant="contained"
+                color="primary"
+              >
+                Search
+              </Button>
+            </AppBar>
             <Form currentId={currentId} setCurrentId={setCurrentId} />
             <Paper elevation={6}>
               <Pagination />
