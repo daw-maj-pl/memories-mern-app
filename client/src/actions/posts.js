10a218246fff706d9c3b79699a5a4411406a5df7
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-05-23 10:52:44 +0200

    Dispatch actions of START_LOADING and END_LOADING types

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -2,6 +2,8 @@ import * as api from '../api/index.js';
 import {
   FETCH_ALL,
   FETCH_BY_SEARCH,
+  START_LOADING,
+  END_LOADING,
   CREATE,
   UPDATE,
   DELETE,
@@ -10,9 +12,11 @@ import {
 
 export const getPosts = page => async dispatch => {
   try {
+    dispatch({ type: START_LOADING });
     const { data } = await api.fetchPosts(page);
 
     dispatch({ type: FETCH_ALL, payload: data });
+    dispatch({ type: END_LOADING });
   } catch (error) {
     console.log(error.message);
   }
@@ -20,11 +24,13 @@ export const getPosts = page => async dispatch => {
 
 export const getPostsBySearch = searchQuery => async dispatch => {
   try {
+    dispatch({ type: START_LOADING });
     const {
       data: { data }
     } = await api.fetchPostsBySearch(searchQuery);
 
     dispatch({ type: FETCH_BY_SEARCH, payload: data });
+    dispatch({ type: END_LOADING });
   } catch (error) {
     console.log(error);
   }
@@ -32,6 +38,7 @@ export const getPostsBySearch = searchQuery => async dispatch => {
 
 export const createPost = post => async dispatch => {
   try {
+    dispatch({ type: START_LOADING });
     const { data } = await api.createPost(post);
 
     dispatch({ type: CREATE, payload: data });
