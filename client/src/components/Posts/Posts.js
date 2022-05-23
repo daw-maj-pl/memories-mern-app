0a3a4b8fe5a5a8ac787a8254fea44b6c644a9b68
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-05-23 10:44:21 +0200

    Destructure posts object and add optional chaining

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -4,9 +4,9 @@ import { useSelector } from 'react-redux';
 import Post from './Post/Post';
 
 const Posts = ({ setCurrentId }) => {
-  const posts = useSelector(state => state.posts);
+  const { posts } = useSelector(state => state.posts);
 
-  return !posts.length ? (
+  return !posts?.length ? (
     <CircularProgress />
   ) : (
     <Grid container alignItems="stretch" spacing={3}>
