3cef2b3b267f977b4c75dc26d7686b93923edfae
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-05-24 09:12:03 +0200

    Implement START_LOADING and END_LOADING cases

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -4,11 +4,17 @@ import {
   CREATE,
   UPDATE,
   DELETE,
-  LIKE
+  LIKE,
+  START_LOADING,
+  END_LOADING
 } from '../constants/actionTypes';
 
-export default (state = {}, action) => {
+export default (state = { isLoading: true, posts: [] }, action) => {
   switch (action.type) {
+    case START_LOADING:
+      return { ...state, isLoading: true };
+    case END_LOADING:
+      return { ...state, isLoading: false };
     case FETCH_ALL:
       return {
         ...state,
