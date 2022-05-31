42f74f6d0fafeed59d8314c8f8588b844f1891f1
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-05-31 13:16:42 +0200

    Create getPost controller

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -23,6 +23,34 @@ export const getPosts = async (req, res) => {
   }
 };
 
+export const getPostsBySearch = async (req, res) => {
+  const { searchQuery, tags } = req.query;
+
+  try {
+    const title = new RegExp(searchQuery, 'i');
+
+    const posts = await PostMessage.find({
+      $or: [{ title }, { tags: { $in: tags.split(',') } }]
+    });
+
+    res.json({ data: posts });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const getPost = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const post = await PostMessage.findById(id);
+
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
 export const createPost = async (req, res) => {
   const post = req.body;
 
@@ -38,22 +66,6 @@ export const createPost = async (req, res) => {
     res.status(201).json(newPost);
   } catch (error) {
     res.status(409).json({ message: error.message });
-  }
-};
-
-export const getPostsBySearch = async (req, res) => {
-  const { searchQuery, tags } = req.query;
-
-  try {
-    const title = new RegExp(searchQuery, 'i');
-
-    const posts = await PostMessage.find({
-      $or: [{ title }, { tags: { $in: tags.split(',') } }]
-    });
-
-    res.json({ data: posts });
-  } catch (error) {
-    res.status(404).json({ message: error.message });
   }
 };
 
